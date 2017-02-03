758dac0e6955758938aa033ee5025d4d5a89e960
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-02-03 12:32:04 -0500

    updating controller

--- budget_app_solution/controllers/credits.js
+++ budget_app_solution/controllers/credits.js
@@ -8,15 +8,6 @@ router.get('/', function(req, res){
       res.status(200).send(credit);
     });
 });
-// // GET
-// function getAll(request, response) {
-//   Credits.find(function(error, credit) {
-//     if(error) response.status(404).send(error);
-//
-//     response.status(200).send(credit);
-//   }).select('-__v');
-// }
-//
 
 // POST: ADD A NEW BUDGET AMOUNT
 router.post("/", function(req, res){
@@ -29,46 +20,5 @@ router.post("/", function(req, res){
   });
 });
 
-
-
-
-//
-// // GET
-// function getCredits(request, response) {
-//   var id = request.params.id;
-//
-//   Credits.findById({_id: id}, function(error, credit) {
-//     if(error) response.status(404).send(error);
-//
-//     response.status(200).send(credit);
-//   }).select('-__v');
-// }
-//
-// function updateCredits(request, response) {
-//   var id = request.params.id;
-//
-//   Credits.findById({_id: id}, function(error, credit) {
-//     if(error) response.status(404).send(error);
-//
-//     if(request.body.name) credit.name = request.body.name;
-//     if(request.body.lightsaber) credit.lightsaber = request.body.lightsaber;
-//
-//     credit.save(function(error) {
-//       if(error) response.status(500).send(error);
-//
-//       response.status(200).send(credit);
-//     });
-//   }).select('-__v');
-// }
-//
-// function removeCredits(request, response) {
-//   var id = request.params.id;
-//
-//   Credits.remove({_id: id}, function(error) {
-//     if(error) response.status(404).send(error);
-//
-//     response.status(200);
-//   }).select('-__v');
-// }
 
 module.exports = router;
